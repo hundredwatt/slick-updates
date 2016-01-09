dc5249b912006d6d6a4daaf05108225586a98fe0
Jason Nochlin <hundredwatt@users.noreply.github.com>
2016-01-08 22:50:43 -0500

    Serve action cable requests in-app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+
   resources :questions
   resources :update_forms
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
 end
