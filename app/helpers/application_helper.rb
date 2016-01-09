b9888c505969ee29a31d3cf207490ac7e61b68f1
Jason Nochlin <hundredwatt@users.noreply.github.com>
2016-01-09 12:59:33 -0500

    Set page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  # View helper to set the page title
+  def title(page_title)
+    content_for :title, page_title.to_s
+  end
+
   def publicize(update_form)
     PublicUpdateForm.new(update_form)
   end
